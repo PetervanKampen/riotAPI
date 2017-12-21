cc41a149be29433138544aadd401fb7b0bc11b94
Peter van Kampen <peter.van.kampen@outlook.com>
2017-12-21 01:46:26 +0100

    Fixed

--- riotAPI/src/riotapi/URLstore.java
+++ riotAPI/src/riotapi/URLstore.java
@@ -15,10 +15,6 @@ public class URLstore
 
     private String APIkey = "";
     
-    public URLstore()
-    {
-        APIkey = fileReader();
-    }
 
     public String summoner(String name, String region)
     {
@@ -37,28 +33,4 @@ public class URLstore
         APIkey = key;
     }
 
-    public static String fileReader()
-    {
-        StringBuilder sb = new StringBuilder();
-        String strLine = "";
-        try
-        {
-            BufferedReader br = new BufferedReader(new FileReader("C:/Users/Public/Desktop/RIOT API KET.txt"));
-            while (strLine != null)
-            {
-                sb.append(strLine);
-                sb.append(System.lineSeparator());
-                strLine = br.readLine();
-                return strLine;
-            }
-            br.close();
-        } catch (FileNotFoundException e)
-        {
-            System.err.println("File not found");
-        } catch (IOException e)
-        {
-            System.err.println("Unable to read the file.");
-        }
-        return "";
-    }
 }
